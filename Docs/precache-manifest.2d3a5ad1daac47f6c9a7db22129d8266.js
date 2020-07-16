ed57e9c105f5a52f69ff47c145a83ace208ec382
difo23 <diffozanzan@gmail.com>
2020-07-15 22:17:16 -0400

    Listo para pruduccion

---
+++ Docs/precache-manifest.2d3a5ad1daac47f6c9a7db22129d8266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf061d2285ae25f1e0f754c043ef277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e6fe60d172898b6113",
+    "url": "/static/css/main.660011e7.chunk.css"
+  },
+  {
+    "revision": "b921be1ce5fae2b9591b",
+    "url": "/static/js/2.d2340096.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d2340096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e6fe60d172898b6113",
+    "url": "/static/js/main.495860a8.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
